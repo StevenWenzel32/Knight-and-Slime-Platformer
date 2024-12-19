e9c6a9a5951669d9b437b3583179c1730d3357ae
Steven Wenzel <45366232+StevenWenzel32@users.noreply.github.com>
2024-12-18 17:22:22 -0800

    started falling platform script

---
+++ Assets/Knight and Slime/Scripts/Objects/FallingPlatform.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallingPlatform : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // when touched by a player toggle the gravity of the rigid body component - will need to find a good speed
+    // have the fall speed increase over time something like a slow exponetiatl change
+    
+}
