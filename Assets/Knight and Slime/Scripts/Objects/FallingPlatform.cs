9084fbce50c2503d23b96e4a3d1d983423ddfce0
Steven Wenzel <45366232+StevenWenzel32@users.noreply.github.com>
2024-12-19 17:41:32 -0800

    level 3 is almost done
    added falling platforms. hooked up all buttons and switches. waiting for feedback about falling platforms and holding jump bug. will create another version of this level layout for level 4.

--- Assets/Knight and Slime/Scripts/Objects/FallingPlatform.cs
+++ Assets/Knight and Slime/Scripts/Objects/FallingPlatform.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class FallingPlatform : MonoBehaviour
 {
+    // the rigid body
+    private Rigidbody2D body;
     // Start is called before the first frame update
     void Start()
     {
-        
+        body = this.GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -16,7 +18,16 @@ public class FallingPlatform : MonoBehaviour
         
     }
 
-    // when touched by a player toggle the gravity of the rigid body component - will need to find a good speed
-    // have the fall speed increase over time something like a slow exponetiatl change
-    
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        // Check for collisions with the players 
+        if (collision.gameObject.CompareTag("Slime") || collision.gameObject.CompareTag("Knight"))
+        {
+            Debug.Log("player hit the falling platform");
+            // check the gravity scale
+            if (body.gravityScale == 0){
+                body.gravityScale = 1;
+            }
+        }
+    }
 }
